5acdaa2577a354e0b74acc794498247e2b26a466
DhiDhik15 <putraadhika15@gmail.com>
2021-10-14 11:42:14 +0700

    perbaikan app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
 //
-import Beranda from "./components/home";
-import Navbar from "./components/navbar";
-import List, { ListInv } from "./components/listInv";
-import Form from "./components/form";
+import Navbar from "./components/Navbar";
 import { Container } from "react-bootstrap";
-// import Manajemen from './components/manajemenBuku';
-// const BrowserRouter = require("react-router-dom").BrowserRouter;
-// const Route = require("react-router-dom").Route;
-// const Switch = require("react-router-dom").Switch;
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import Home from "./components/Home";
+import ManajemenInv from "./components/ManajemenInv";
 
 function App() {
+  const [inv, setInv] = useState([]);
+  function storeData(inputInv) {
+    console.log(inputInv);
+    alert("Success");
+  }
+  function updateData(inputInv) {
+    console.log(inv);
+    alert("Berhasil Update");
+  }
+  function deleteData(inv){
+    console.log(inv);
+    alert("Deleted");
+  }
+
   return (
     <div className="App">
       <div className="content">
         <Container>
-          <Navbar />
-          <Beranda />
-          <ListInv />
-          <Form />
+          <BrowserRouter>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact>
+                <Home  menjInv={inv}/>
+              </Route>
+
+              <Route path="/manajemen-inv">
+                <ManajemenInv menjInv={inv} store={storeData} update={updateData} delete={deleteData}/>
+              </Route>
+            </Switch>
+          </BrowserRouter>
         </Container>
       </div>
     </div>
