1963a49d840862727a11b1403858bd40c839ef33
DhiDhik15 <putraadhika15@gmail.com>
2021-10-15 12:48:56 +0700

    perbaikan server

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,25 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
 
 /*parse requests of content-type - application/json*/
 app.use(express.json());
+app.use(cors());
+app.use(morgan("combined"));
 
 /*parse requests of content-type - application/x-www-form-urlencoded*/
-app.use(express.urlencoded({
-  extended: true
-}));
-const route = require('./routes/route');
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+const route = require("./routes/route");
 route(app);
 
 /*PORT*/
 const DPORT = process.env.DEFAULT_PORT || 3001;
-// sslServer.listen(PORT, () => {
 app.listen(DPORT, () => {
   console.log(`--->Service on PORT ${DPORT}.<---`);
 });
@@ -24,10 +29,10 @@ app.listen(DPORT, () => {
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 27017;
 
-mongoose.connect('mongodb://localhost:27017/inventory', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/books");
 mongoose.connection
   .once("open", function () {
-    console.log(`--->DB Service on PORT ${PORT}<---`);
+    console.log(`--->DB on PORT ${PORT}<---`);
   })
   .on("error", function (error) {
     console.log("error is:", error);
