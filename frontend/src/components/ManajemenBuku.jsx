b7921202cafdb3866324d98c7f7f605a8c653fbc
DhiDhik15 <putraadhika15@gmail.com>
2021-10-17 21:41:14 +0700

    buat ulang

--- frontend/src/components/ManajemenBuku.jsx
+++ frontend/src/components/ManajemenBuku.jsx
@@ -1,209 +1,86 @@
-import React from "react";
-import { useState } from "react";
+import axios from "axios";
+import React, { Component } from "react";
+import { API_URL } from "../apis/ListApi";
+import {
+  BsFillBagXFill,
+  BsFillPencilFill,
+  BsFillCartPlusFill,
+  BsGrid3X3Gap,
+} from "react-icons/bs";
 
-function ManajemenBuku({ bookList, store, update, remove }) {
-  const [inputBook, setInputBook] = useState();
-  console.log(bookList);
-  const [form, setForm] = useState();
+/*function button*/
+// function showCreate() {
+//     setForm("create");
+//   }
 
-  /*function handling*/
-  function handleJudul(event) {
-    setInputBook({ ...inputBook, judul: event.target.value });
+export default class ManajemenBuku extends Component {
+  constructor(props) {
+    super(props);
+    /*save data in array state*/
+    this.state = {
+      bookList: [],
+    };
   }
-  function handlePengarang(event) {
-    setInputBook({ ...inputBook, pengarang: event.target.value });
-  }
-  function handleHarga(event) {
-    setInputBook({ ...inputBook, harga: event.target.value });
-  }
-  function handleStok(event) {
-    setInputBook({ ...inputBook, stok: event.target.value });
-  }
-  /*end function*/
 
-  /*function button*/
-  function showCreate() {
-    setForm("create");
+  /*get data*/
+  componentDidMount() {
+    axios
+      .get(API_URL + "get")
+      .then((res) => {
+        console.log(res.data.book);
+        this.setState({ bookList: res.data.book });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  function showEdit(book) {
-    setInputBook(book);
-    setForm("edit");
-  }
-  /*end function*/
 
-  /*Function CRUD*/
-  function submitAdd(event) {
-    event.preventDefault();
-    store(inputBook);
-  }
-  function submitChange(event) {
-    event.preventDefault();
-    update(inputBook);
-    setForm("");
-  }
-  function deleteBook(book) {
-    remove(book);
-  }
-  /*end function*/
-
-  /*add data*/ 
-  return (
-    <div className="container mt-3">
-      {form === "create" && (
-        <div id="formTambah">
-          <form className="form-row" onSubmit={submitAdd}>
-            <div className="col-3">
-              <input
-                type="text"
-                name="judul"
-                className="form-control mx-2"
-                placeholder="Judul"
-                onChange={handleJudul}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="text"
-                name="pengarang"
-                className="form-control mx-2"
-                placeholder="Pengarang"
-                onChange={handlePengarang}
-              />
-            </div>
-            <div className="col-2">
-              <input
-                type="number"
-                name="harga"
-                className="form-control mx-2"
-                placeholder="Harga"
-                onChange={handleHarga}
-              />
-            </div>
-            <div className="col-2">
-              <input
-                type="number"
-                name="stok"
-                className="form-control mx-2"
-                placeholder="Stok"
-                onChange={handleStok}
-              />
-            </div>
-            <br />
-            <div className="col-2">
-              <input
-                type="submit"
-                className="btn btn-primary ml-5"
-                value="Simpan "
-              />
-            </div>
-          </form>
-        </div>
-      )}
-
-  {/* edit data */}
-      {form === "edit" && (
-        <div id="fomUbah">
-          <form className="form-row" onSubmit={submitChange}>
-            <div className="col-3 w-10">
-              <input
-                type="text"
-                name="judul"
-                className="form-control mx-2"
-                placeholder="Judul"
-                onChange={handleJudul}
-                value={inputBook.judul}
-              />
-            </div>
-            <div className="col-3 w-10">
-              <input
-                type="text"
-                name="pengarang"
-                className="form-control mx-2"
-                placeholder="Pengarang"
-                onChange={handlePengarang}
-                value={inputBook.pengarang}
-              />
-            </div>
-            <div className="col-2 w-10">
-              <input
-                type="number"
-                name="harga"
-                className="form-control mx-2"
-                placeholder="Harga"
-                onChange={handleHarga}
-                value={inputBook.harga}
-              />
-            </div>
-            <div className="col-2 w-10">
-              <input
-                type="number"
-                name="stok"
-                className="form-control mx-2"
-                placeholder="Stok"
-                onChange={handleStok}
-                value={inputBook.stok}
-              />
-            </div>
-            <br />
-            <div className="col-2">
-              <input
-                type="submit"
-                className="btn btn-success ml-5"
-                value="Update "
-              />
-            </div>
-          </form>
-        </div>
-      )}
-
-  {/* list data */}
-      <div id="daftarBuku">
-        <hr />
-        <h5>Manajemen Buku</h5>
-        <hr />
-        <button className="btn btn-primary m-2" onClick={showCreate}>
-          Tambah
-        </button>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Judul</th>
-              <th>Pengarang</th>
-              <th>Harga</th>
-              <th>Stok</th>
-              <th>##</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bookList.map((book, index) => (
-              <tr key={index}>
-                <td> {index + 1} </td>
-                <td> {book.judul} </td>
-                <td> {book.pengarang} </td>
-                <td> {book.harga} </td>
-                <td> {book.stok} </td>
-                <td>
-                  <button
-                    className="btn btn-warning m-2"
-                    onClick={() => showEdit(book)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => deleteBook(book)}
-                  >
-                    Hapus
-                  </button>
-                </td>
+  render() {
+    return (
+      <div className="container mt-3">
+        {/* list buku */}
+        <div id="daftarBuku">
+          <hr />
+          <h4>
+            <BsGrid3X3Gap /> Manajemen Buku
+          </h4>
+          <hr />
+          <button className="btn btn-primary m-2">
+            <BsFillCartPlusFill />
+          </button>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Judul</th>
+                <th>Pengarang</th>
+                <th>Harga</th>
+                <th>Stok</th>
+                <th>##</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.bookList.map((book, index) => (
+                <tr key={index}>
+                  <td> {index + 1} </td>
+                  <td> {book.judul} </td>
+                  <td> {book.pengarang} </td>
+                  <td> {book.harga} </td>
+                  <td> {book.stok} </td>
+                  <td>
+                    <button className="btn btn-warning m-2">
+                      <BsFillPencilFill />
+                    </button>
+                    <button className="btn btn-danger">
+                      <BsFillBagXFill />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default ManajemenBuku;
