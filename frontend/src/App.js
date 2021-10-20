a5a0d61a9ae25b82b67ffda625543cba8098d1a4
DhiDhik15 <putraadhika15@gmail.com>
2021-10-20 11:48:28 +0700

    clean code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,9 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import Beranda from "./components/Beranda";
-import ManajemenBuku from "./components/ManajemenBuku";
-import axios from "axios";
+import Beranda from "./pages/Beranda";
+import ManajemenBuku from "./pages/ManajemenBuku";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  /*function GET data*/
-  useEffect(() => {
-    retrieveData();
-  }, []);
-
-  function retrieveData() {
-    axios
-      .get("http://localhost:3001/get")
-      .then((response) => {
-        setBooks(response.data.data);
-        console.log("setBooks" + response.data.data);
-      });
-  }
-
-  /*function POST data*/
-  function storeData(inputBook) {
-    axios({
-      method: "POST",
-      url: "http://localhost:3001/add",
-    })
-      .then((res) => {
-        setBooks((prevBooks) => [...prevBooks, inputBook]);
-        alert("Success");
-      })
-      .catch((error) => {
-        console.log(error.response.data);
-      });
-  }
-
-  /*function PUT data*/
-  function updateData(inputBook) {
-    axios
-      .put("http://localhost/3001/put/" + inputBook._id, inputBook)
-      .then((res) => {
-        retrieveData();
-        alert("updated");
-      })
-      .catch((error) => {
-        console.log(error.response.data);
-      });
-  }
-
-  /*function DELETE data*/
-  function deleteData(book) {
-    axios
-      .delete("http://localhost:3001/del/" + book._id)
-      .then(() => {
-        retrieveData();
-        alert("Deleted");
-      })
-      .catch((error) => {
-        console.log(error.response.data);
-      });
-  }
-  /*end function*/
 
   return (
     <div className="App">
@@ -72,17 +13,11 @@ function App() {
 
           <Switch>
             <Route path="/" exact>
-              <Beranda 
-                bookList={books} />
+              <Beranda />
             </Route>
 
             <Route path="/manajemen-buku">
-              <ManajemenBuku
-                bookList={books}
-                store={storeData}
-                update={updateData}
-                remove={deleteData}
-              />
+              <ManajemenBuku />
             </Route>
           </Switch>
         </BrowserRouter>
